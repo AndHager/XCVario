d28439c353cd8707e6b13dddee814b9d95925c5c
Eckhard Völlm <e.voellm@mail.de>
2022-07-14 20:51:16 +0200

    average over pressure and temperature at selftest for SPL sensor type, and fix logging in I2Cbus

--- components/I2Cbus/src/I2Cbus.cpp
+++ components/I2Cbus/src/I2Cbus.cpp
@@ -256,8 +256,7 @@ esp_err_t I2C::writeBytes(uint8_t devAddr, uint8_t regAddr, size_t length, const
         char str[length*5+1];
         for (size_t i = 0; i < length; i++)
             sprintf(str+i*5, "0x%s%X ", (data[i] < 0x10 ? "0" : ""), data[i]);
-        I2CBUS_LOG_RW("[port:%d, slave:0x%X] Write %d bytes to register 0x%X, data: %s",
-            port, devAddr, length, regAddr, str);
+        ESP_LOGI(TAG,"[port:%d, slave:0x%X] Write %d bytes to register 0x%X, data: %s", port, devAddr, length, regAddr, str);
     }
 #endif
 #if defined CONFIG_I2CBUS_LOG_ERRORS
@@ -323,7 +322,7 @@ esp_err_t I2C::readBytes(uint8_t devAddr, uint8_t regAddr, size_t length, uint8_
     		char str[length*5+1];
     		for (size_t i = 0; i < length; i++)
     			sprintf(str+i*5, "0x%s%X ", (data[i] < 0x10 ? "0" : ""), data[i]);
-    		I2CBUS_LOG_RW("[port:%d, slave:0x%X] Read_ %d bytes from register 0x%X, data: %s", port, devAddr, length, regAddr, str);
+    		ESP_LOGI(TAG,"[port:%d, slave:0x%X] Read_ %d bytes from register 0x%X, data: %s", port, devAddr, length, regAddr, str);
     	}
     }
 #endif
